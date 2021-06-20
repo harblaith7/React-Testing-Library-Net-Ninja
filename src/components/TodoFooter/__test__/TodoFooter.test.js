ecfecd2802390b6fa81e6b70e911a695e2a6c860
Laith Harb <laithharb@Laiths-iMac.local>
2021-06-20 10:00:08 -0400

    Complete assertion lesson

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -15,9 +15,79 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
 it('should render the correct amount of incomplete tasks', () => {
     render(
         <MockTodoFooter 
-        numberOfIncompleteTasks={5}
+          numberOfIncompleteTasks={5}
         />
     );
     const pElement = screen.getByText(/5 tasks left/i);
     expect(pElement).toBeInTheDocument();
 });
+
+it('should render "task" when the number of incomplete tasks is one', () => {
+  render(
+      <MockTodoFooter 
+        numberOfIncompleteTasks={1}
+      />
+  );
+  const pElement = screen.getByText(/1 task left/i);
+  expect(pElement).toBeInTheDocument();
+});
+
+// it('p element should be truthy when the number of incomplete tasks is one', () => {
+//   render(
+//       <MockTodoFooter 
+//         numberOfIncompleteTasks={1}
+//       />
+//   );
+//   const pElement = screen.getByText(/1 task left/i);
+//   expect(pElement).toBeTruthy();
+// });
+
+// it('"task" should be visible when the number of incomplete tasks is one', () => {
+//   render(
+//       <MockTodoFooter 
+//         numberOfIncompleteTasks={1}
+//       />
+//   );
+//   const pElement = screen.getByText(/1 task left/i);
+//   expect(pElement).toBeVisible();
+// });
+
+// it('should contain p tag with correct text', () => {
+//   render(
+//       <MockTodoFooter 
+//         numberOfIncompleteTasks={1}
+//       />
+//   );
+//   const pElement = screen.getByText(/1 task left/i);
+//   expect(pElement).toContainHTML('p');
+// });
+
+// it('should render correct text content', () => {
+//   render(
+//       <MockTodoFooter 
+//         numberOfIncompleteTasks={1}
+//       />
+//   );
+//   const pElement = screen.getByText(/1 task left/i);
+//   expect(pElement).toHaveTextContent("1 task left");
+// });
+
+// it('should render correct text content', () => {
+//   render(
+//       <MockTodoFooter 
+//         numberOfIncompleteTasks={1}
+//       />
+//   );
+//   const pElement = screen.getByText(/1 task left/i);
+//   expect(pElement).not.toBeFalsy();
+// });
+
+// it('should render correct text content', () => {
+//   render(
+//       <MockTodoFooter 
+//         numberOfIncompleteTasks={1}
+//       />
+//   );
+//   const pElement = screen.getByText(/1 task left/i);
+//   expect(pElement.textContent).toBe("1 task left");
+// });
