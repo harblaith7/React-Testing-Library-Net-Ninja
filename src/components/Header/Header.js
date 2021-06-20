3577138ea06b0e77e30ff7cb856732e9b7e586ef
Laith Harb <laithharb@Laiths-iMac.local>
2021-06-20 09:21:40 -0400

    Complete querying DOM elements lesson

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,5 +4,10 @@ import "./Header.css"
 export default function Header({
     title
 }) {
-    return <h1 className="header">{title}</h1>
+    return (
+        <>
+            <h1 title="Header" className="header">{title}</h1>
+            {/* <h3 data-testid="header-2" className="header">Hello</h3> */}
+        </>
+    )
 }
