0b79318c97f3526ab36f8593f354372cc05bc969
Laith Harb <laithharb@Laiths-iMac.local>
2021-06-21 08:54:07 -0400

    Add before & after each hooks

--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -13,8 +13,21 @@ const MockFollowersList = () => {
 describe("FollowersList", () => {
 
     beforeEach(() => {
+        // console.log("RUNS BEFORE EACH TEST")
         jest.mock("../../../__mocks__/axios")
     })
+
+    // beforeAll(() => {
+    //     console.log("RUNS ONCE BEFORE ALL TESTS")
+    // })
+
+    // afterEach(() => {
+    //     console.log("RUNS AFTER EACH TEST")
+    // })
+
+    // afterAll(() => {
+    //     console.log("RUNS ONCE AFTER ALL TESTS")
+    // })
 
     it('should fetch and render input element', async () => {
         render(
@@ -30,7 +43,6 @@ describe("FollowersList", () => {
         );
     
         const followerDivElement = await screen.findByTestId(`follower-item-0`)
-        screen.debug()
         expect(followerDivElement).toBeInTheDocument();
     });
 })
