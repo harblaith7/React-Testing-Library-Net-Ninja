d403d5eda676c2ba6b204c403a5f966dcc9b0a81
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-11-06 12:37:05 +0100

    get, find, query

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,5 +4,8 @@ import "./Header.css"
 export default function Header({
     title
 }) {
-    return <h1 className="header">{title}</h1>
+    return <>
+    <h1 className="header" data-testid="header-1">{title}</h1>
+    <h3 title="header" className="header">cats</h3>
+    </>
 }
